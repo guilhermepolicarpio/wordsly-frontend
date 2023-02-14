0fd36014c2c3f2c21c3e868ac74d25a9124a7a92
Guilherme <guilhermepolicarpio@gmail.com>
2023-02-14 14:25:58 -0300

    feat: added dificulty level explain and layout fix

--- src/components/OptionBox.js
+++ src/components/OptionBox.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 
 export default function OptionBox() {
 
-    const { configData,setConfigData } = useContext(ConfigContext);
+    const { configData, setConfigData } = useContext(ConfigContext);
 
     const options = [
         { value: "1", label: "Easy" },
@@ -21,27 +21,49 @@ export default function OptionBox() {
         console.log(`Option selected:`, selectedOption);
     };
 
-    console.log(configData)
     return (
+        <>
         <OptionContainer>
-            <p>Selecionar o nível de dificuldade</p>
+            <p>Select difficulty level</p>
             <>
-            <Select options={options} onChange={handleChange} autoFocus={true} />
-            <Link to="/menu">
-                <Button type="submit" >Salvar</Button>
-            </Link>
+                <Select options={options} width='180px' onChange={handleChange} autoFocus={true} />
+                <LevelExplain>
+                    <p>Currently</p>
+                    <p>Difficulty level: {configData.label}</p>
+                    <p>Number of blocks on game: {configData.value}</p>
+                </LevelExplain>
+              
             </>
         </OptionContainer>
+         <Link to="/menu">
+         <Button type="submit" >Salvar</Button>
+        </Link>
+        </>
 
     );
 }
 
 export const OptionContainer = styled.div`
 display: flex;
-flex-direction: column;
 align-items: center;
 justify-content: space-around;
 width: 145vh;
 height: 88vh;
+`
+
+export const LevelExplain = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-around;
+width: 55vh;
+height: 14vh;
+background-color: white;
+border-radius: 37%;
+
+p:first-child{
+    margin-bottom: 1.2vh;
+    color: gray;
+}
 
 `
